d35dbbab653a24e04e3247aa0678270caa4c6acd
Lilith-codes <nagasaililith@gmail.com>
2024-08-28 01:55:25 -0400

    My second commit

---
+++ BasicPython.py
@@ -0,0 +1,11 @@
+l = [[23, 24, 26, 28, 54, 567, 789, 45], [234, 345, 446, 67, 89, 90, 456, 776], [54, 43, 544, 999, "Sneha"], "Vladamir", "Kim Mama"]
+print(l[0:3])
+
+# Accessing the target list inside a nested lists through indexification technique
+print(l[0:3][2])
+print(l[:][2])
+print([45, 6456, 544])
+print(l[::][2])
+
+
+
---
+++ TuplesOperations.py
@@ -0,0 +1,20 @@
+#Lists (Basic Example)
+
+#Suggest a morning drink for Sneha
+
+Tea = ["Milk", "Sugar", "Tea powder"]
+print("Morning drink:", Tea)
+
+# Add Honey into the List of Tea
+
+Tea.append("Honey")
+print("Sneha wants to drink:", Tea)
+
+# Remove Sugar from the List of Tea
+
+Tea.remove("Sugar")
+print("Morning drink:", Tea)
+
+
+
+
